aba37fedf9b1184e1c3e7aaa39f24e1d4ca0e92f
Peter Anderson <pete.anderson@gmail.com>
2023-12-13 15:11:36 +0100

    add: demo

--- components/Joke/index.js
+++ components/Joke/index.js
@@ -1,26 +1,33 @@
 import { useEffect, useState } from "react";
+import useSWR from "swr";
 
-function useFetch(url) {
-  const [data, setData] = useState();
+// function useFetch(url) {
+//   const [data, setData] = useState();
+//   useEffect(() => {
+//     async function startFetching() {
+//       console.log("fetching data");
+//       const response = await fetch(url);
+//       const newData = await response.json();
 
-  useEffect(() => {
-    async function startFetching() {
-      const response = await fetch(url);
-      const newData = await response.json();
+//       setData(newData);
+//     }
 
-      setData(newData);
-    }
+//     startFetching();
+//   }, [url]);
 
-    startFetching();
-  }, [url]);
+//   return data;
+// }
 
-  return data;
-}
+// const fetcher = (url) => fetch(url).then((response) => response.json());
 
 export default function Joke() {
   const [id, setId] = useState(0);
 
-  const data = useFetch(`https://example-apis.vercel.app/api/bad-jokes/${id}`);
+  // const data = useFetch(`https://example-apis.vercel.app/api/bad-jokes/${id}`);
+
+  const { data, isLoading, isValidating, error } = useSWR(
+    `https://example-apis.vercel.app/api/bad-jokes/${id}`
+  );
 
   function handlePrevJoke() {
     setId(data.prevId);
@@ -30,7 +37,11 @@ export default function Joke() {
     setId(data.nextId);
   }
 
-  if (!data) {
+  if (error) {
+    return <h1>Oops, something went wrong</h1>;
+  }
+
+  if (isLoading) {
     return <h1>Loading...</h1>;
   }
 
